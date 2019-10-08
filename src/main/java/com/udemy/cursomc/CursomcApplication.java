06fbe1da6f66cd9070cbcf4d9d5490d5719c23aa
Jhonny Ulisses Silva <1745962@TCS.com>
2019-10-08 16:07:37 -0300

    Produto e associação ManyToMany

--- src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.udemy.cursomc;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.domain.Produto;
 import com.udemy.cursomc.repositories.CategoriaRepository;
+import com.udemy.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +15,10 @@ import java.util.Arrays;
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
-	private CategoriaRepository repository;
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -21,10 +26,22 @@ public class CursomcApplication implements CommandLineRunner{
 
 	public void run(String... args) throws Exception {
 
-		Categoria cat1 = new Categoria(1,"Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
 
-		repository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 }
---
+++ src/main/java/com/udemy/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.udemy.cursomc.repositories;
+
+import com.udemy.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
