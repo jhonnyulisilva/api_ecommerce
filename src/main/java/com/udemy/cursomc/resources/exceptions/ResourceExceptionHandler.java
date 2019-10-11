e73490f16b8d7ab9d4043bd47d303afb5ef60b30
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-10-11 17:37:31 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/udemy/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
--- src/main/java/com/udemy/cursomc/services/ClienteService.java
+++ src/main/java/com/udemy/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.udemy.cursomc.services;
 
+import com.udemy.cursomc.DTO.ClienteDTO;
 import com.udemy.cursomc.domain.Cliente;
 import com.udemy.cursomc.repositories.ClienteRepository;
+import com.udemy.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
@@ -20,6 +28,40 @@ public class ClienteService {
                     + ", Tipo " + Cliente.class.getName());
         }
         return obj;
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try{
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possivel excluir porque a entidades relacionadas!");
+        }
+
+    }
+
+    public List<Cliente> findAll() {
+        return repo.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Cliente fromDto(ClienteDTO    objDto) {
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
     }
 
 }
