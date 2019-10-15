976d4755bedf9c52ce37820bec703cad3d763a2b
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-10-15 12:56:56 -0300

    Inserindo Pedido

--- src/main/java/com/udemy/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComCartao.java
@@ -1,10 +1,12 @@
 package com.udemy.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.udemy.cursomc.domain.Enums.EstadoPagamento;
 
 import javax.persistence.Entity;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
     private static final long serialVersionUID = 1L;
