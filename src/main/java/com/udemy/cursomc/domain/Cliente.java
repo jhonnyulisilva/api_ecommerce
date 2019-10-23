9af3b520a61035eaba7dfd9fc7dd25098e5a1e0e
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-10-23 16:53:58 -0300

    Salvando perfil de usuarios na base de Dados

--- src/main/java/com/udemy/cursomc/domain/Cliente.java
+++ src/main/java/com/udemy/cursomc/domain/Cliente.java
@@ -1,11 +1,14 @@
 package com.udemy.cursomc.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.udemy.cursomc.domain.Enums.Perfil;
 import com.udemy.cursomc.domain.Enums.TipoCliente;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Period;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente implements Serializable {
@@ -31,12 +34,16 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "PERFIS")
+    private Set<Integer> perfis = new HashSet<>();
+
     @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
     public Cliente() {
-
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String email, String cpfOuCpnj, TipoCliente tipo, String senha) {
@@ -46,6 +53,7 @@ public class Cliente implements Serializable {
         this.cpfOuCnpj = cpfOuCpnj;
         this.tipo = (tipo == null) ? null: tipo.getCod();
         this.senha = senha;
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Integer getId() {
@@ -110,6 +118,22 @@ public class Cliente implements Serializable {
 
     public void setTelefones(Set<String> telefones) {
         this.telefones = telefones;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x ->
+        {
+            try {
+                return Perfil.toEnum(x);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        perfis.add(perfil.getCod());
     }
 
     public List<Pedido> getPedidos() {
--- src/main/java/com/udemy/cursomc/services/DBService.java
+++ src/main/java/com/udemy/cursomc/services/DBService.java
@@ -2,6 +2,7 @@ package com.udemy.cursomc.services;
 
 import com.udemy.cursomc.domain.*;
 import com.udemy.cursomc.domain.Enums.EstadoPagamento;
+import com.udemy.cursomc.domain.Enums.Perfil;
 import com.udemy.cursomc.domain.Enums.TipoCliente;
 import com.udemy.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -95,16 +96,20 @@ public class DBService {
         cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
         Cliente cli1 = new Cliente(null, "Maria Silva", "jhonnyulisses2014@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("123"));
+        Cliente cli2 = new Cliente(null, "Jhonny Ulisses", "jhonnyulisilva@gmail.com", "08739317935", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("123"));
+        cli2.addPerfil(Perfil.ADMIN);
 
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+        cli2.getTelefones().addAll(Arrays.asList("33264667", "991383319"));
 
         Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
         Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+        Endereco e3 = new Endereco(null, "Rua Floripa", "1231", null, "Copacabana", "89587214", cli2, c1);
 
-        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2, e3));
 
-        clienteRepository.saveAll(Arrays.asList(cli1));
-        enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
